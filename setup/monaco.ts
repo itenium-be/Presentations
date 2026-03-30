1e86b4f7b98e9b115fa0bbb5fbcfc7d4ea8cc45e
Laoujin <woutervs@hotmail.com>
2026-03-30 02:15:17 +0200

    fira code fallback to ibm plex

--- setup/monaco.ts
+++ setup/monaco.ts
@@ -2,7 +2,7 @@ import { defineMonacoSetup } from '@slidev/types'
 
 export default defineMonacoSetup(() => {
   return {
-    fontFamily: "'Fira Code'",
+    fontFamily: "'Fira Code', 'IBM Plex Mono', monospace",
     fontLigatures: true,
     fontSize: 16,
     'editor.fontLigatures': true,
